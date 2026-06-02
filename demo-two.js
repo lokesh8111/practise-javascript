2f98e8f19d899135f0ce961540a27c6531a638ef
Pullagura Lokesh <lokeshpullagura8111@gmail.com>
2026-06-02 21:32:44 +0530

    Add files via upload

--- demo-two.js
+++ demo-two.js
@@ -1,6 +1,6 @@
-process.stdout.write("P ");
+process.stdout.write("P "); //prints and stays on the same line
 process.stdout.write("Lokesh ");
-console.log("Hello world");
+console.log("Hello world"); //prints and moves to the next line
 console.log('Hello world');
 console.log(9);
 console.log("Lokesh", 9);
