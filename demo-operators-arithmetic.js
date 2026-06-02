903c64e8059a2cdba055a2e9e3328024da8252b8
Pullagura Lokesh <lokeshpullagura8111@gmail.com>
2026-06-02 21:33:34 +0530

    Add files via upload

---
+++ demo-operators-arithmetic.js
@@ -0,0 +1,18 @@
+let a = 10;
+let b = 5;
+console.log("Addition:", a + b); // 15
+console.log("Subtraction:", a - b); // 5
+console.log("Multiplication:", a * b); // 50
+console.log("Division:", a / b); // 2
+console.log("Modulus:", a % b); // 0 returns the remainder of the division
+console.log("Exponentiation:", a ** b); // 100000 returns the result of raising a to the power of b
+let c = 3;
+console.log("post Increment:", c++); // 3 returns the value before incrementing
+console.log("After Increment:", c); // 4
+console.log("pre Increment:", ++c); // 5 increments first and then returns the value
+console.log("After Pre Increment:", c); // 5
+let d = 3;
+console.log("post Decrement:", d--); // 3 returns the value before decrementing
+console.log("After Decrement:", d); // 2
+console.log("pre Decrement:", --d); // 1 decrements first and then returns the value
+console.log("After Pre Decrement:", d); // 1
